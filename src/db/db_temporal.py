c2d6444728ed2fcec94250a66c4f8a884212b927
Christian <christianpefer@gmail.com>
2024-09-29 19:33:12 +0200

    refactor: removed unused code

--- src/db/db_temporal.py
+++ src/db/db_temporal.py
@@ -1,13 +1,7 @@
-from src.service.rss_feed_fetcher import RSSFeedFetcher
-from src.service.parser.lne_parser import LNEParserEvent
 from typing import List
+
 from src.model.event import Event
 
-RSS_FEED_URL = "https://www.lne.es/rss/section/2014085"
-rss_fetcher = RSSFeedFetcher()
 
-db_temporal = {}
-
-async def get_lne_rss_events() ->List[Event]: #para probar, hay que hacer un flow para que esto llegue a bbdd
-    events = await rss_fetcher.get_events(RSS_FEED_URL, LNEParserEvent)
-    return events
+async def get_events() -> List[Event]:
+    return []
