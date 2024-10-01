f7bdb430c2e8fcb068035b9577e251ca0a5e0007
Christian <christianpefer@gmail.com>
2024-10-01 20:29:31 +0200

    feat: consume AgendaGijon API

--- src/controller/event_controller.py
+++ src/controller/event_controller.py
@@ -1,9 +1,12 @@
 from typing import List
 
 from src.model.event import Event
+from src.service.api_consumer_service import ApiConsumerService
+from src.service.parser import AgendaGijonParser
 
 
 class EventController:
     @staticmethod
     async def get_events() -> List[Event]:
-        return []
+        url = "https://drupal.gijon.es/es/listado_eventos_tes3/?_format=json"
+        return ApiConsumerService.consume(url, AgendaGijonParser())
---
+++ src/service/parser/__init__.py
@@ -0,0 +1,4 @@
+from .agenda_gijon_parser import AgendaGijonParser
+from .event_parser import EventParser
+
+__all__ = ["EventParser", "AgendaGijonParser"]
