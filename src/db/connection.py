24ccf051cdba56638031853bad968f38ba52f712
AnaGciaSchz <anags.clm@gmail.com>
2024-10-11 18:41:14 +0200

    Adding connection to DB

---
+++ src/db/connection.py
@@ -0,0 +1,27 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+class Connection:
+    _instance = None
+    _engine = None
+    _Session = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            url = os.getenv("TURSO_DATABASE_URL")
+            auth_token = os.getenv("TURSO_AUTH_TOKEN")
+            connection_string = f"{url}?auth_token={auth_token}"
+
+            cls._instance = super(Connection, cls).__new__(cls)
+            cls._engine = create_engine(connection_string)
+            cls._Session = sessionmaker(bind=cls._engine)
+        return cls._instance
+
+    def get_session(self):
+        """Devuelve una nueva sesión para interactuar con la base de datos."""
+        return self._Session()
+
+    def __del__(self):
+        self.close()
+
