ae6391943b75f74c4351cdcd3ad5e6d046451734
Christian <christianpefer@gmail.com>
2025-01-22 21:14:36 +0100

    chore: to handle cors

--- src/main.py
+++ src/main.py
@@ -1,9 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.router import event_router, organizer_router
 
 app = FastAPI()
 favicon_path = 'favicon.ico'
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(event_router.router)
 app.include_router(organizer_router.router)
